25b6032f48f54c8633723aaf01473640cb3c146c
omkar tawade <92808540+omkar1127@users.noreply.github.com>
2024-08-27 23:53:52 +0530

    Update Customer_Data_analysis.sql

--- Customer_Data_analysis.sql
+++ Customer_Data_analysis.sql
@@ -1,5 +1,6 @@
 select *  from dbo.support_data
 
+--Let's create a view so that only necessary columns can be used for Analysis.
 create view customer as
 select [channel_name],[category],[Sub-category],[Customer Remarks],[order_date_time]
 ,[Issue_reported at],[issue_responded],[Response_Time],[Survey_response_Date],[Customer_City],[Product_category]
@@ -23,7 +24,7 @@ select [Agent Shift],round(avg(Response_Time),0) as Response_Time
 from customer
 group by [Agent Shift]
 
---how many percet agents have the highest and lowest CSAT scores?
+--how many percentage agents have the highest and lowest CSAT scores?
 WITH total AS (
     SELECT COUNT(DISTINCT Agent_name) AS total_count
     FROM customer
@@ -45,39 +46,43 @@ FROM total, highest_score, lowest_score;
 
 
 --How does agent performance vary under different supervisors?
-select Supervisor,round(avg([CSAT Score]),2) as Avg_csat_score,COUNT(Agent_name) as No_of_agents from customer
+select Supervisor,round(avg([CSAT Score]),2) as Avg_csat_score,COUNT(Agent_name) as No_of_agents 
+from customer
 group by Supervisor
 order by avg_csat_score desc;
 
 --What are the most common issues reported by customers?
-select top 10 category,[Sub-category] as issues,COUNT(category) as Frequency from customer
+select top 10 category,[Sub-category] as issues,COUNT(category) as Frequency 
+from customer
 group by [sub-category],category
 order by Frequency desc
 
 --How does customer satisfaction correlate with different product categories?
-select category,round(avg([CSAT Score]),2) from customer
+select category,round(avg([CSAT Score]),2) 
+from customer
 group by category
 
 --What is the average order value associated with different customer complaints?
-select category,AVG(cast(Item_price as int)) as average_order_value from customer
+select category,AVG(cast(Item_price as int)) as average_order_value 
+from customer
 where Item_price is not null
 group by category
 order by average_order_value desc
 
 --How do issue response times differ by customer city?
--- Using a subquery to calculate hours and minutes
-SELECT Customer_City,hr,minu
+SELECT Customer_City,hr,in_min
 FROM (	
 		SELECT Customer_City,ROUND(CAST(Response_Time AS INT) / 60, 0) AS hr,
-        CAST(CAST(Response_Time AS INT) % 60 AS INT) AS minu
+        CAST(CAST(Response_Time AS INT) % 60 AS INT) AS in_minu
 		FROM customer
 		WHERE Customer_City IS NOT NULL) AS Subquery
-WHERE hr > 0 OR minu > 0
-ORDER BY hr DESC, minu DESC;
+WHERE hr > 0 OR in_minu > 0
+ORDER BY hr DESC, in_minu DESC;
 
 
 --What trends can be identified in customer remarks over time?
-select top 10 [Customer Remarks],count([Customer Remarks]) as frequency from customer 
+select top 10 [Customer Remarks],count([Customer Remarks]) as frequency 
+from customer 
 where [Customer Remarks] is not null
 group by [Customer Remarks]
 order by frequency desc
